f29c1b9709aec3e1cf5b6b76f16e15ce89f622e6
marthatazvivinga480-cyber <marthatazvivinga480@gmail.com>
2026-06-24 22:36:43 +0200

    Add JavaScript beginner challenge solutions

---
+++ Exercise 1-2.js
@@ -0,0 +1,83 @@
+//1.Declare a variable called name and store your name in it
+let name = ("Martha" );
+console.log("Hello "  + name);
+
+//2.Ask for a person's age and display it.
+let age = 45;
+console.log("Your age is "  + age);
+
+
+
+// 3.Create variables for length and width and calculate the area of a rectangle.
+
+length = 8;
+Width = 5;
+const area = (length* Width)
+console.log (area);
+
+//4.adding two numbers
+const num1 = 20
+const num2 = 40
+const sum = num1 + num2;
+console.log(sum);
+
+//5. favouriteColor
+
+const favouriteColor = "White"
+console.log("my favourite color is  " + favouriteColor)
+
+// 6.variables for first and last name combined
+const firstName = "Martha"
+const LastName = "Mukucha"
+console.log( firstName +" "+ LastName);
+
+// 7.Find the remainder when 17 is divided by 3.
+const num3 = 17;
+const num4= 3;
+const modulus = num3 % num4
+console.log(modulus)
+
+
+//8.converting a string into a number
+const num5 = "25"
+num5 == 25
+console.log (num5)
+
+//9.swap the values of two variables
+let num6 = 20;
+let num7 = 50;
+
+let temp = num6;
+num6 = num7;
+num7= temp;
+
+console.log("num6 =", num6);
+console.log("num7 =", num7);
+
+//10.Calculate the average of three numbers.
+const num8 = 10;
+const num9 = 20;
+const num10 = 30;
+
+const average = (num8 + num9 + num10) / 3;
+
+console.log(average);
+
+//14.temperature in Celsius and convert it to Fahrenheit.
+
+const celsius =  35;
+const fahreheit = (celsius * 9 / 5 ) + 32;
+console.log("fahreheit=" , fahreheit );
+
+//15.Display the square of a number
+
+const num = 4;
+
+const square = num ** 2;
+
+console.log("Square =", square);
+
+/* Conditional Statements */
+const username = "Mary";
+let age2= 22;
+console.log(age2);
---
+++ exercise 3.js
@@ -0,0 +1,114 @@
+/* Conditional Statements: Positive or Negative */ 
+/*const num = Number(prompt(" enter a number:"));
+if (num >= 0) {
+    console.log("Positive");
+} else{
+    console.log("Negative");
+}
+
+/*   even or odd  numbers */
+// const num1 = Number(prompt(`enter a number:`))
+// if (num1 % 2 === 0) {
+//     console.log("even")
+// } else{
+//     console.log("odd");
+// }
+
+// /* eligible to vote (18+) */
+
+// const age = Number(prompt("Enter age:"));
+// if (age >= 18){
+//     console.log("Eligible to vote");
+// } else{
+//     console.log("Not eligible");
+// }
+
+// // const mark = Number(prompt("Enter mark"));
+// // if ( mark >= 50) {
+// //     console.log("passed");
+// // } else{
+// //     console.log("Failed");
+// // }
+
+// /*leap year*/
+// /*const year = Number(prompt("Enter year:"));
+
+// if (year % 4 === 0) {
+//     console.log("Leap year");
+// } else {
+//     console.log("Not a leap year");
+// }
+
+// /* number is divisible by 5*/
+// const num2 = Number(prompt("Enter a number:"));
+
+// if (num2 % 5 === 0) {
+//     console.log("Divisible by 5");
+// } else {
+//     console.log(" Not Divisible by 5");
+//     }
+
+//     /* assigning grades */
+ 
+// const mark = Number(prompt("Enter grades:"));
+//    switch(true){
+
+//  case(mark > 90 && mark <=100): 
+//     console.log("A");
+//     break;
+
+// case(mark > 80 && mark <=89): 
+//     console.log("B");
+// break;
+
+// case(mark > 70 &&  mark <=79): 
+//     console.log("C");
+//     break;
+
+// case(mark >69 && mark>=69):
+//      console.log("Fail");
+//   break;
+//    }
+
+
+// const num = prompt("put your number")
+// switch (true) {
+//     case(num <= 0):
+//         console.log("negative")
+//         break;
+
+//     case (num >=0):
+//         console.log("Postive")
+//         break;
+
+//     default:
+//         console.log("Not a number");
+// }*/
+
+
+const num3 = Number(prompt("put your number:"));
+switch (true) {
+    case (num3 % 2=== 0):
+        alert("even number");
+        break;
+
+    case (num3 % 2 !== 0):
+        alert("odd number")
+        break;
+    default:
+        alert("Not a number");    
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+  
+ 
