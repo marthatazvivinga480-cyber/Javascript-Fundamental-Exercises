52d5e069bd4eddfbcc8c3ed47cd2a984ebe515f8
marthatazvivinga480-cyber <marthatazvivinga480@gmail.com>
2026-06-30 19:54:46 +0200

    numbers divible by both 3 and 5

--- exercise 3.js
+++ exercise 3.js
@@ -338,3 +338,10 @@ for (let i = 1; i <= 5; i++) {
 };
 
 
+//40.Print all numbers between 1 and 100 that are divisible by both 3 and 5.
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i);
+    }
+};
+
