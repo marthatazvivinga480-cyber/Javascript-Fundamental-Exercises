4196d78c7fd24225e7f05773b0ec328adc4f517b
marthatazvivinga480-cyber <marthatazvivinga480@gmail.com>
2026-06-30 19:11:25 +0200

    even and odd number

--- exercise 3.js
+++ exercise 3.js
@@ -1,7 +1,7 @@
 
 // Conditional Statements
 
-// 16.Positive or Negative 
+// 16.Determine whether a number is positive or negative.p 
 const num = prompt("put your number")
 switch (true) {
     case(num <= 0):
@@ -15,3 +15,36 @@ switch (true) {
     default:
         console.log("Not a number");
 };
+
+//17. even or odd  numbers 
+const num3 = Number(prompt("put your number:"));
+switch (true) {
+    case (num3 % 2=== 0):
+        alert("even number");
+        break;
+
+    case (num3 % 2 !== 0):
+        alert("odd number")
+        break;
+    default:
+        alert("Not a number")};
+
+
+//18.Compare two numbers and print the larger one
+
+const num4 = Number(prompt("Enter first number:"));
+const num5 = Number(prompt("Enter second number:"));
+
+switch (true) {
+    case num4 > num5:
+        console.log(num4 + " is larger");
+        break;
+
+    case num5 > num4:
+        console.log(num5 + " is larger");
+        break;
+
+    default:
+        console.log("Both numbers are equal");
+};
+
