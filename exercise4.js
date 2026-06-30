5baddf45ad67f1e53ef82a4d1ead94684b60c8ae
marthatazvivinga480-cyber <marthatazvivinga480@gmail.com>
2026-06-30 15:06:41 +0200

    Added forEach array examples

--- exercise4.js
+++ exercise4.js
@@ -4,7 +4,13 @@ const numbers = [5, 10, 15, 20, 25];
 numbers.forEach(number => {
     console.log(number);
 });
-    
+ 
+//2.Print every number in an array
+const names = ["Alice", "Bob", "Charlie", "David"];
+names.forEach(function(name) {
+    console.log(name)
+
+});
 
 
 
