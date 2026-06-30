b55b32fbfde241c318c8e1d1e71350f884b9885f
marthatazvivinga480-cyber <marthatazvivinga480@gmail.com>
2026-06-30 19:46:09 +0200

    sum of numbers 1 to 100

--- exercise 3.js
+++ exercise 3.js
@@ -285,3 +285,13 @@ while (multiple <= 10) {
     multiple++;
 };
 
+
+//35. Calculate the sum of numbers from 1 to 100.
+let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+    sum += i;
+};
+
+console.log("Sum of numbers from 1 to 100:", sum);
+
