c584f79dfc8cd615d5523c094a4c5233ff185e47
marthatazvivinga480-cyber <marthatazvivinga480@gmail.com>
2026-06-30 19:43:55 +0200

    print even number up to 20

--- exercise 3.js
+++ exercise 3.js
@@ -269,3 +269,9 @@ for (let i = 10; i >= 1; i--) {
     console.log(i);
 };
 
+//33.Print all even numbers between 1 and 20
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+};
