4b93d08087e182216c08483d1f98cc360418b8cc
marthatazvivinga480-cyber <marthatazvivinga480@gmail.com>
2026-06-30 19:56:26 +0200

    pattern

--- exercise 3.js
+++ exercise 3.js
@@ -345,3 +345,15 @@ for (let i = 1; i <= 100; i++) {
     }
 };
 
+
+
+// Nested loop statement
+//41. Print:
+for (let i = 1; i <= 5; i++) {
+    let row = "";
+    for (let j = 1; j <= 5; j++) {
+        row += i;
+    }
+    console.log(row);
+};
+
