2042b3263e052967c96cdbde8fb3dfe6581aa0f0
marthatazvivinga480-cyber <marthatazvivinga480@gmail.com>
2026-06-30 19:58:08 +0200

    increasing pattern

--- exercise 3.js
+++ exercise 3.js
@@ -367,3 +367,13 @@ for (let i = 1; i <= 5; i++) {
     console.log(row);
 };
 
+
+//43.print increasing number pattern
+ 
+ for (let i = 1; i <= 5; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+        row += j;
+    }
+    console.log(row);
+};
